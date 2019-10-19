cd7105e6dee7862e2649ea107cfc10ec5a40460e
angelRacancoj <orlan_1397rg@hotmail.com>
2019-10-19 11:48:19 -0600

    Se agregaron los cambios necesarios a la Base de datos y se arreglaron los controladores de Client y Supply

--- controller/Supply/newSupply.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require 'supply.php';
-	$nameSupply=POST['name_supply'];
-	$dateExpiry=POST['date_expiry'];
-	$cuantityAvailable=POST['cuantity_available'];
-	$securitySheet=POST['secure_sheet'];
-	$measureId=POST['measure_id'];
-	newSupply($nameSupply,$dateExpiry,$cuantityAvailable,$measureId,$securitySheet);
-?>
--- controller/Supply/supplyController.php
+++ controller/Supply/supplyController.php
@@ -1,55 +1,78 @@
 <?php
-
+	require_once("../../model/src/Entity/Supply.php");
 	function newSupply($name,$date_expire,$quantity_available,$measure){
-		//Rafa
+		//Agregar en la base de datos
 	}
 
 	function newSupply($name,$date_expire,$quantity_available,$measure,$secure_sheet){
-		//Rafa
+		//Agregar en la base de datos
 	}	
 
 	function modifyName($name){
-		//rafa
+		//actualizar en base de datos
 	}
 
 	function modifyDateExpired($date_expired){
-
+		//actualizar en base de datos
 	}
 
 	function modifyQuantityAvailable($quantity_available){
-
-	}
-
-	function modifyMeasure($measure){
-
+		//actualizar en base de datos
 	}
 
 	function getByName($name){
-
+		//buscar en base de datos
+        //devolver Insumos
 	}
 
-	function getById($id){
-
+	function modifyMeasure($measure){
+		//actualizar en base de datos
 	}
 
-
-	function getBuId($id){
-
+	function getByCode($code){
+		//buscar en base de datos
+		//devolver Insumos
 	}
 
 	function getAvailableByCode($available,$code){
-
+		//Esta es la estructura del funcionamiento, si lo consideras necesario la podes eliminar
+		if (isset($available)) {
+			if ($available == true) {
+				//Elegir los insumos con cantidad mayor a 0 y q la fecha de expiracion sea mayor a la actual
+			} else {
+				//Elegir los insumos con cantidad igual a 0 y q la fecha de expiracion sea menor a la actual
+			}
+		} else {
+			return getByCode($code);
+		}
 	}
 
 	function getAvailableByName($available,$name){
-
+		//Esta es la estructura del funcionamiento, si lo consideras necesario la podes eliminar
+		if (isset($available)) {
+			if ($available == true) {
+				//Elegir los insumos con cantidad mayor a 0 y q la fecha de expiracion sea mayor a la actual
+			} else {
+				//Elegir los insumos con cantidad igual a 0 y q la fecha de expiracion sea menor a la actual
+			}
+		} else {
+			return getByName($code);
+		}
 	}
 
 	function getByNameAndCode($name,$code){
+		//buscar en base de datos
+		//devolver Insumos
+	}
 
+	function getByAvailableNameCode($available,$code,$name){
+		//la logica implementada en:
+		getAvailableByName();
+		getAvailableByCode();
 	}
 
 	function getAllSuppies(){
-
+		//buscar en base de datos
+		//devolver Insumos
 	}
 ?>
