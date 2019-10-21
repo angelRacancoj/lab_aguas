0ff918083930fe84bc90d8d3ba6eee67aa442c48
angelRacancoj <orlan_1397rg@hotmail.com>
2019-10-21 10:22:10 -0600

    Continua con el trabajo el la GUI y backend

---
+++ data_base/db_data/client.sql
@@ -0,0 +1,7 @@
+INSERT INTO `water_laboratory`.`CLIENT` (`dpi_client`, `name_client`, `phone_client`, `costum_client_id`) VALUES (1234567890901,'Hansel Perez', '12345678', 1);
+INSERT INTO `water_laboratory`.`CLIENT` (`dpi_client`, `name_client`, `phone_client`, `costum_client_id`) VALUES (1112223330901,'Javier Pac', '12345698', 1);
+INSERT INTO `water_laboratory`.`CLIENT` (`dpi_client`, `name_client`, `phone_client`, `costum_client_id`) VALUES (2223334440901,'Erick Coti', '25896325', 5);
+INSERT INTO `water_laboratory`.`CLIENT` (`dpi_client`, `name_client`, `phone_client`, `costum_client_id`) VALUES (3334445550901,'Carlos Cayax', '15874268', 4);
+INSERT INTO `water_laboratory`.`CLIENT` (`dpi_client`, `name_client`, `phone_client`, `costum_client_id`) VALUES (2027218180901,'Marianna Alvarado', '35715946', 2);
+INSERT INTO `water_laboratory`.`CLIENT` (`dpi_client`, `name_client`, `phone_client`, `costum_client_id`) VALUES (4445556660901,'Eugenia Garcia', '13971397', 4);
+INSERT INTO `water_laboratory`.`CLIENT` (`dpi_client`, `name_client`, `phone_client`, `costum_client_id`) VALUES (5556667770901,'Jeanneth Baten', '48692179', 3);
