b3fc0d9f9c331fdfe6b5382f323679f0ddc66c58
angelRacancoj <orlan_1397rg@hotmail.com>
2019-10-21 21:48:35 -0600

    Se ha conectado exitosamente el archivo PHP con el HTML usando funciones

--- view/Client/conect.php
+++ view/Client/conect.php
@@ -1,4 +1,9 @@
  <?php
+$servername = "localhost";
+$username = "root";
+$password = "ex=d/dx=ex";
+$dbname = "water_laboratory";
+
  if(isset($_POST['add'])){
 	echo "<p>Hola</p>";
 	printClient();
@@ -24,30 +29,7 @@ function printClient(){
 function hola_mundo(){
 	echo "<h1>Hola Mundo</h1>";
 }
-/*
-$servername = "localhost";
-$username = "root";
-$password = "ex=d/dx=ex";
-$dbname = "water_laboratory";
 
-if(isset($_POST['addClient'])){
-	echo "Hola3";
-	if (isset($_POST['dpi'],$name=$_POST['client_name'],$phone=$_POST['phone'],$costum=$_POST['costum'])) {
-		$dpi=$_POST['dpi'];
-		$name=$_POST['client_name'];
-		$direction=$_POST['direction'];
-		$department=$_POST['department'];
-		$company=$_POST['company'];
-		$phone=$_POST['phone'];
-		$extra_phone=$_POST['extra_phone'];
-		$fax=$_POST['fax'];
-		$email=$_POST['email'];
-		$web=$_POST['web'];
-		$costum=$_POST['costum'];
-   		printClient($dpi,$name,$direction,$department,$company,$phone,$extra_phone,$fax,$email,$web,$costum);
-	}
-} 
-	echo "<p>Hola1</p>";
 function createClient($dpi,$name,$phone,$costum_id){
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
@@ -56,7 +38,7 @@ function createClient($dpi,$name,$phone,$costum_id){
 	    die("Connection failed: " . $conn->connect_error);
 	}
 
-	$sql = "INSERT INTO `water_laboratory`.`CLIENT` (`dpi_client`, `name_client`, `phone_client`, `costum_client_id`) VALUES (5556667770901,'Jeanneth Baten', '48692179', 3);";
+	$sql = "INSERT INTO `water_laboratory`.`CLIENT` (`dpi_client`, `name_client`, `phone_client`, `costum_client_id`) VALUES ($dpi,$name,$direction,$department,$company,$phone,$extra_phone,$fax,$email,$web,$costum);";
 
 	if ($conn->query($sql) === TRUE) {
 	    echo "New record created successfully";
@@ -65,6 +47,6 @@ function createClient($dpi,$name,$phone,$costum_id){
 	}
 
 	$conn->close();
+	//printClient($dpi,$name,$direction,$department,$company,$phone,$extra_phone,$fax,$email,$web,$costum);
 }
-*/
 ?> 
